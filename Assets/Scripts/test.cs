3805aa17602d8bdf35878c9cc1a366cbff0839c6
chyk22056 <146048010+chyk22056@users.noreply.github.com>
2024-12-20 15:46:38 +0900

    add kime

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class test
+{
+    void Start()//練習
+    {
+        Debug.Log("練習");
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/test2.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class test2 : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Debug.Log("練習 start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("練習　update");
+    }
+}
