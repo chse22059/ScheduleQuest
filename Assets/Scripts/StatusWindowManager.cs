fa27d78b4aebb8c1d5d2b4694d9ed85452ae8f5c
chkt22058 <145964587+chkt22058@users.noreply.github.com>
2024-12-24 04:14:31 +0900

    Add "StatusSO" File and "Player" in resources folder

---
+++ Assets/Scripts/StatusWindowManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class StatusWindowManager : MonoBehaviour
+{
+    [SerializeField] StatusSO statusSO;
+
+    [SerializeField] TextMeshProUGUI HPValue;
+    [SerializeField] TextMeshProUGUI MPValue;
+    [SerializeField] TextMeshProUGUI ATKValue;
+    [SerializeField] TextMeshProUGUI EQUIPValue;
+    [SerializeField] TextMeshProUGUI GOLDValue;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        HPValue.GetComponent<TextMeshProUGUI>().text = statusSO.HP.ToString();
+        MPValue.GetComponent<TextMeshProUGUI>().text = statusSO.MP.ToString();
+        ATKValue.GetComponent<TextMeshProUGUI>().text = statusSO.ATK.ToString();
+        EQUIPValue.GetComponent<TextMeshProUGUI>().text = statusSO.EQUIP.ToString();
+        GOLDValue.GetComponent<TextMeshProUGUI>().text = statusSO.GOLD.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
